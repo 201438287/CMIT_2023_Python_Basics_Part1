ab51c18c1e7701ec218ad70873b2140d460972bc
201438287 <55025392+201438287@users.noreply.github.com>
2023-06-15 08:45:21 +0100

    Add files via upload
    Typos corrected

--- P7_EllipticIntegralKk.py
+++ P7_EllipticIntegralKk.py
@@ -1,17 +1,18 @@
 import numpy as np
 import math
+#Trapezium
 def Khalf(theta):
     d=4-math.pow(np.sin(theta),2)
     y=2/(math.pow(d,1/2))
     return y
-#the last one does not count
-T=np.linspace(0,np.pi/2, num=500)[:-1]
+
+T=np.linspace(0,np.pi/2, num=500)
 #increment in theta
 dt=T[1]-T[0]
 K=[]
 for t in T:
     K.append(Khalf(t))
 #integral 
-I=sum(K)*dt
+I=sum(K)*dt-(T[0]+T[1])/2
 print(I)
 
